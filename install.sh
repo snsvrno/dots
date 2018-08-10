c2821337ac5c74230bf1a5a446c4f738427dfc1a
snsvrno <snsvrno@tuta.io>
2018-08-09 23:03:55 -0600

    fix(install-script): Checks for valid lines in list
    Validates list and adds comments, #, and empty line support.
    
    <footer> BREAKING CHANGE:

--- install.sh
+++ install.sh
@@ -8,8 +8,13 @@ while read LINE; do
   NEW_PATH=${COMMANDS[1]}
   COMMAND_1=`echo "${NEW_PATH/"~"/$HOME}"`
 
-  if [[ ! -e $COMMAND_1 ]]; then
-    printf "  Installing %s\n" ${COMMANDS[0]}
-    ln ${COMMANDS[0]} $COMMAND_1 
+  if [[ ${LINE:0:1} != "#" ]] ; then 
+    if [[ ${#COMMANDS[@]} == 2 ]]; then
+      if [[ ! -e $COMMAND_1 ]]; then
+        printf "  Installing %s\n" ${COMMANDS[0]}
+        ln ${COMMANDS[0]} $COMMAND_1 
+      fi
+    fi
   fi
+
 done < list

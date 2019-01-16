71204633dd914ed535adaf81fe1e0e342ab904e1
snsvrno <snsvrno@tuta.io>
2019-01-16 11:16:12 -0700

    fix: gpg & git fix

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,2 @@
-# android / rust / variables
-set -x ANDROID_HOME /home/snsvrno/.android/android-sdk
-set -x ANDROID_NDK {$ANDROID_HOME}/ndk-bundle
-set -x ANDROID_NDK_CARGO /home/snsvrno/.config/android-ndk
+# gpg & git
+set -x GPG_TTY (tty)

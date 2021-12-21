7cdcc495d96fe7fa0bb64df4763ca58b2f16c334
snsvrno <snsvrno@tuta.io>
2021-12-21 11:34:29 -0700

    adds plugins.lua to nvim

---
+++ nvim/plugins.lua
@@ -0,0 +1,57 @@
+local packer = require("packer")
+
+local function startup()
+
+	use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
+
+	use 'sheerun/vim-polyglot'
+--	use 'jdonaldson/vaxe'
+
+-- THEMES
+	use 'folke/tokyonight.nvim'
+--	use 'projekt0n/github-nvim-thme'
+--	use 'https://git.sr.ht/~novakane/kosmikoa/nvim'
+	use 'rafamadriz/neon'
+	use 'catppuccin/nvim'
+--	use 'glepnir/zephyr-nvim'
+	use 'sainnhe/sonokai'
+	use 'Mofiqul/dracula.nvim'
+
+	use 'airblade/vim-gitgutter'
+-- 	use { 'romgrk/barbar.nvim', requires = 'kyazdani42/nvim-web-devicons' }
+	
+	use { 'kyazdani42/nvim-tree.lua', 
+		requires = 'kyazdani42/nvim-web-devicons', 
+		config = function() require('nvim-tree').setup { } end 
+	}
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+
+	use { "folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			}
+  	end
+	}
+	use 'junegunn/fzf'
+	use 'junegunn/fzf.vim'
+
+	use 'numToSTr/FTerm.nvim'
+
+
+-- language stuff
+--	use 'neovim/nvim-lspconfig'
+	use { 'neoclide/coc.nvim', branch = "release" }
+
+	use 'majutsushi/tagbar'
+
+end
+
+return packer.startup(startup)

f8dedec13c34464f2e747630c6cbb043e2a2a88f
snsvrno <snsvrno@tuta.io>
2022-03-30 08:48:32 -0600

    tweaking luasnip, setting keymaps

--- .config/nvim/lua/config/basic.lua
+++ .config/nvim/lua/config/basic.lua
@@ -10,7 +10,8 @@ vim.opt.autoindent = true
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2 				-- the number of spaces a tab displays.
-vim.opt.softtabstop=0
+vim.opt.softtabstop = 0
 vim.opt.termguicolors = true
+vim.opt.scrolloff = 10				-- shows at least 10 lines on the screen at once when scrolling.
 
 vim.cmd[[set clipboard^=unnamed,unnamedplus]]
--- .config/nvim/lua/config/plugins/luasnip.lua
+++ .config/nvim/lua/config/plugins/luasnip.lua
@@ -8,7 +8,28 @@ return function(use)
 		},
 
 		config = function()
+			require("luasnip").config.set_config {
+				history = true,
+				updateevents = "TextChanged,TextChangedI",
+				enable_autosnippets = true,
+			}
 
+
+			--vim.api.nvim_set_keymap("i", "<c-l>", ":lua local ls = require ('luasnip'); if ls.expand_or_jumpable() then ls.expand_or_jump() end<CR>", { silent = true })
+			--vim.api.nvim_set_keymap("i", "<c-h>", ":lua local ls = require('luasnip'); if ls.jumpable(-1) then ls.jump(-1) end<CR>", { silent = true })
+			--vim.api.nvim_set_keymap("i", "<c-k>", ":lua local ls = require('luasnip'); if ls.choice_active() then ls.change_choice(1) end<CR>", { silent = true })
+			--vim.api.nvim_set_keymap("i", "<c-j>", ":lua local ls = require('luasnip'); if ls.choice_active() then ls.change_choice(-1) end<CR>", { silent = true })
+
+			vim.cmd [[imap <silent> <c-l> <cmd> lua local ls = require ('luasnip'); if ls.expand_or_jumpable() then ls.expand_or_jump() end <Cr>]]
+			vim.cmd [[imap <silent> <c-h> <cmd> lua local ls = require ('luasnip'); if ls.jumpable(-1) then ls.jump(-1) end <Cr>]]
+			--vim.cmd [[inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>]]
+
+			--vim.cmd [[snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>]]
+			--vim.cmd [[snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>]]
+
+			--vim.cmd [[imap <silent><expr> <C-Tab> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<Cr>']]
+			vim.cmd [[imap <silent> <c-j> <cmd> lua local ls = require('luasnip'); if ls.choice_active() then ls.change_choice(1) end <Cr>]]
+			vim.cmd [[imap <silent> <c-k> <cmd> lua local ls = require('luasnip'); if ls.choice_active() then ls.change_choice(-1) end <Cr>]]
 		end,
 
 	}

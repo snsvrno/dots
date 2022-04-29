a88956c19ec479bc62fd6cd25d3e5898701927e9
snsvrno <snsvrno@tuta.io>
2022-04-28 18:55:11 -0600

    removes polybar

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-polybar laptop -r 2>&1 | tee -a /tmp/polybar.log & disown
-polybar left -r 2>&1 | tee -a /tmp/polybar.log & disown
-polybar center -r 2>&1 | tee -a /tmp/polybar.log & disown
-polybar right -r 2>&1 | tee -a /tmp/polybar.log & disown

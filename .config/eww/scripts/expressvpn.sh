88dcabf14e3d51fb8e0d99855bd38ba2a885886b
snsvrno <snsvrno@tuta.io>
2022-10-20 16:15:08 -0600

    eww: adds icon class, fixes icons

--- .config/eww/scripts/expressvpn.sh
+++ .config/eww/scripts/expressvpn.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 vpn_status=$(expressvpn status | grep -E "Connected to" | cut -d"-" -f2 | cut -d' ' -f2,3,4,5,6)
+
+if [ $vpn_status == ""]; then 
+	vpn_status="Not Connected" 
+fi
+
 echo "$vpn_status"
